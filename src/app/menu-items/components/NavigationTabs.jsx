813f0d60e2e1a83223f0d01379ed1388d4ff2eba
hpcao299 <caohoangphuc2909@gmail.com>
2025-01-08 21:08:05 +0700

    fix default value of navigation tabs component

--- src/app/menu-items/components/NavigationTabs.jsx
+++ src/app/menu-items/components/NavigationTabs.jsx
@@ -1,26 +1,31 @@
+'use client';
+
 import routes from '@/config/routes';
 import { Tabs } from '@chakra-ui/react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { BiDish } from 'react-icons/bi';
 import { LuLayoutGrid, LuList } from 'react-icons/lu';
 
 const NavigationTabs = () => {
+    const pathname = usePathname();
+
     return (
-        <Tabs.Root defaultValue="dashboard" variant="enclosed">
+        <Tabs.Root defaultValue={pathname} variant="enclosed">
             <Tabs.List>
-                <Tabs.Trigger value="dashboard" asChild>
+                <Tabs.Trigger value={routes.menuItems} asChild>
                     <Link unstyled="true" href={routes.menuItems}>
                         <LuLayoutGrid />
                         Dashboard
                     </Link>
                 </Tabs.Trigger>
-                <Tabs.Trigger value="categories" asChild>
+                <Tabs.Trigger value={routes.menuCategories} asChild>
                     <Link unstyled="true" href={routes.menuCategories}>
                         <LuList />
                         Categories
                     </Link>
                 </Tabs.Trigger>
-                <Tabs.Trigger value="dishes" asChild>
+                <Tabs.Trigger value={routes.menuDishes} asChild>
                     <Link unstyled="true" href={routes.menuDishes}>
                         <BiDish />
                         Dishes
