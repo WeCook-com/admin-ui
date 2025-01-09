decac204963dd1fe456650dbb732f13398df8fa9
hpcao299 <caohoangphuc2909@gmail.com>
2025-01-09 20:44:51 +0700

    search input for menu items dishes page

--- src/app/menu-items/dishes/page.jsx
+++ src/app/menu-items/dishes/page.jsx
@@ -1,15 +1,29 @@
-import { Button, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Button, Flex, Grid, GridItem, Input, Text } from '@chakra-ui/react';
 import CategoriesList from './components/CategoriesList';
 import DishItem from './components/DishItem';
-import { LuPlus } from 'react-icons/lu';
+import { LuPlus, LuSearch } from 'react-icons/lu';
+import { InputGroup } from '@/components/ui/input-group';
 
 const DishesPage = () => {
     return (
         <>
+            <Text fontSize="22px" fontWeight="600">
+                Dishes
+            </Text>
             <Flex justifyContent="space-between" mb="5">
-                <Text fontSize="22px" fontWeight="600">
-                    Menu Items
-                </Text>
+                <InputGroup
+                    mt="3"
+                    width="25%"
+                    minWidth="350px"
+                    startElement={<LuSearch size={18} />}
+                    bg="white"
+                >
+                    <Input
+                        style={{ paddingLeft: '38px !important' }}
+                        placeholder="Search dish..."
+                        size="md"
+                    />
+                </InputGroup>
                 <Button variant="subtle" colorPalette="blue">
                     <LuPlus /> Add new Item
                 </Button>
